d646e94613903f0f15b24277f70250db13976ccf
Abhay D <a.deshpande012@gmail.com>
2020-06-25 23:46:32 -0700

    Added Java code to write gcode to drawer

---
+++ src/main/java/com/coolioasjulio/whiteboard/Playground.java
@@ -0,0 +1,65 @@
+package com.coolioasjulio.whiteboard;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.Enumeration;
+import java.util.List;
+import purejavacomm.CommPortIdentifier;
+import purejavacomm.SerialPort;
+import purejavacomm.SerialPortEvent;
+import purejavacomm.SerialPortEventListener;
+
+public class Playground implements SerialPortEventListener {
+
+    public static void main(String[] args) throws Exception {
+//        Playground p = new Playground();
+//        while (true) {
+//            Playground.class.wait();
+//        }
+
+        Enumeration<CommPortIdentifier> portList = CommPortIdentifier.getPortIdentifiers();
+        System.out.println(portList.hasMoreElements());
+        while (portList.hasMoreElements()) {
+            System.out.println("Has more elements");
+            CommPortIdentifier portId = portList.nextElement();
+            System.out.println(portId.getName());
+        }
+    }
+
+
+    private final SerialPort port;
+    private final BufferedReader in;
+    private final PrintStream out;
+
+    public Playground() throws Exception {
+        port = (SerialPort) CommPortIdentifier.getPortIdentifier("COM3").open("blah", 1000);
+        port.setSerialPortParams(9600, SerialPort.DATABITS_8,
+                SerialPort.STOPBITS_1,
+                SerialPort.PARITY_NONE);
+
+        in = new BufferedReader(new InputStreamReader(port.getInputStream()));
+        out = new PrintStream(port.getOutputStream());
+
+        port.addEventListener(this);
+        port.notifyOnDataAvailable(true);
+    }
+
+    public void serialEvent(SerialPortEvent serialPortEvent) {
+        if (serialPortEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+            try {
+                System.out.println(in.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public synchronized void close() {
+        if (port != null) {
+            port.removeEventListener();
+            port.close();
+        }
+    }
+}
---
+++ src/main/java/com/coolioasjulio/whiteboard/WhiteboardDrawer.java
@@ -0,0 +1,92 @@
+package com.coolioasjulio.whiteboard;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import purejavacomm.CommPortIdentifier;
+import purejavacomm.SerialPort;
+
+public class WhiteboardDrawer implements AutoCloseable, Closeable {
+
+    private final SerialPort port;
+    private final BufferedReader in;
+    private final PrintStream out;
+
+    public WhiteboardDrawer(String portName) throws Exception {
+        port = (SerialPort) CommPortIdentifier.getPortIdentifier(portName).open("Drawer", 1000);
+        port.setSerialPortParams(9600, SerialPort.DATABITS_8,
+                SerialPort.STOPBITS_1,
+                SerialPort.PARITY_NONE);
+
+        in = new BufferedReader(new InputStreamReader(port.getInputStream()));
+        out = new PrintStream(port.getOutputStream());
+    }
+
+    public boolean sendCommand(String command) throws IOException {
+        String response;
+        do {
+            out.println(command);
+            out.flush();
+            do {
+                response = in.readLine();
+            } while (response.startsWith("//")); // skip past debug info
+        } while (response.equals("rs"));
+
+        return response.equals("ok");
+    }
+
+    public boolean drawFromFile(File file) throws IOException {
+        BufferedReader fileIn = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = fileIn.readLine()) != null) {
+            line = sanitize(line);
+            line = cleanup(line);
+            if (line.length() > 0) {
+                if (!sendCommand(line)) return false;
+            }
+        }
+
+        return true;
+    }
+
+    public synchronized void close() {
+        if (port != null) {
+            port.removeEventListener();
+            port.close();
+        }
+    }
+
+    /**
+     * Hardware specific cleanup of command. Safely removes select arguments and commands.
+     *
+     * @param command The command to cleanup.
+     * @return A cleaned version of the command. May be an empty string.
+     */
+    private String cleanup(String command) {
+        if (command.equals("G19")) {
+            throw new IllegalArgumentException("All units must be in millimeters!");
+        }
+        switch (command) {
+            case "G21":
+            case "M3":
+            case "M5":
+            case "M2":
+                return "";
+
+            default:
+                return command.replaceAll("F[.\\d\\-]+", "").trim();
+        }
+    }
+
+    private String sanitize(String command) {
+        return command
+                .replaceAll("\\([\\s\\S]*?\\)", "")
+                .replaceAll(";[\\s\\S]*$", "")
+                .replace("%", "")
+                .trim();
+    }
+}
