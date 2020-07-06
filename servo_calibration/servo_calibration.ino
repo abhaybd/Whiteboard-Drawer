ece635cd53b4b314911bad7b6ed9c040ef6d0aad
Abhay D <a.deshpande012@gmail.com>
2020-07-06 04:15:37 -0700

    Moved CAD, added comments

--- servo_calibration/servo_calibration.ino
+++ servo_calibration/servo_calibration.ino
@@ -1,6 +1,12 @@
 #include <Servo.h>
 #include <Wire.h>
 
+/**
+    This script is used to calibrate the servo used to lift and lower the tool.
+    Run this script with the servo plugged in. Type different positions into the terminal, and the servo will go to that position.
+    Find appropriate positions for "up" and "down", and save them.
+*/
+
 Servo servo;
 
 void setup() {
@@ -9,18 +15,22 @@ void setup() {
 }
 
 void loop() {
+    // Wait for data
     while (Serial.available() == 0) {
         delay(10);
     }
 
+    // Read at most 4 bytes from the terminal
     char posTarget[5] = {};
     size_t len = Serial.readBytesUntil('\n', posTarget, 4);
     posTarget[len] = '\0';
 
+    // Parse the recieved number as a long
     long target = strtol(posTarget, nullptr, 10);
     Serial.print("// Recieved target: ");
     Serial.println(target);
 
+    // Set the target for the servo, and wait for it to reach it
     servo.write(target);
     delay(100);
 }
